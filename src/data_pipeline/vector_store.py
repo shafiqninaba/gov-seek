98bdf2e3433d4d7d1710c81deeeec38cec71c39c
Shafiq Ninaba <79973831+shafiqninaba@users.noreply.github.com>
2025-03-03 04:27:38 +0000

    feat: Implement method to populate vector store from Parquet file and create Qdrant collection

--- src/data_pipeline/vector_store.py
+++ src/data_pipeline/vector_store.py
@@ -6,8 +6,9 @@ import os
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams
-from pathlib import Path
+from qdrant_client.http.models import Distance, VectorParams, PointStruct
+import pandas as pd
+from tqdm import tqdm
 
 
 class VectorStore:
@@ -26,32 +27,49 @@ class VectorStore:
             raise ValueError("QDRANT_URL environment variable is not set")
 
         qdrant_url = os.environ.get("QDRANT_URL")
+        self.client = QdrantClient(path=qdrant_url)
 
-        # if path doesnt exist
-        if not Path(qdrant_url).exists():
-            logger.warning(
-                f"Qdrant path does not exist, creating a new Qdrant instance at {qdrant_url}"
-            )
-            self.client = QdrantClient(path=qdrant_url)
-            self.client.create_collection(
+        self.client.create_collection(
+            collection_name="demo_collection",
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+        )
+        self.vector_store = QdrantVectorStore(
+            client=self.client,
+            collection_name="demo_collection",
+            embedding=embeddings,
+        )
+        logger.info(f"Qdrant instance created at {qdrant_url}")
+
+    def populate_vector_store_from_parquet(self, parquet_path: str):
+        """Populate the vector store from a parquet file."""
+
+        df = pd.read_parquet(parquet_path)
+
+        logger.info(f"Populating vector store with {len(df)} rows")
+
+        for _, row in tqdm(
+            df.iterrows(), total=len(df), desc="Populating vector store"
+        ):
+            self.client.upsert(
                 collection_name="demo_collection",
-                vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+                points=[
+                    PointStruct(
+                        id=str(row["uuid"]),
+                        vector=row["embedding"],
+                        payload={
+                            "metadata": {"source": row["link"]},
+                            "page_content": row["text"],
+                        },
+                    )
+                ],
             )
-            self.vector_store = QdrantVectorStore(
-                client=self.client,
-                collection_name="demo_collection",
-                embedding=embeddings,
-            )
-            logger.info(f"Qdrant instance created at {qdrant_url}")
-        else:
-            self.vector_store = QdrantVectorStore.from_existing_collection(
-                embedding=embeddings,
-                collection_name="demo_collection",
-                path=qdrant_url,
-            )
-            logger.info(f"Qdrant instance found at {qdrant_url}")
 
 
 if __name__ == "__main__":
     vector_store = VectorStore()
     logger.info("Vector store initialized")
+
+    vector_store.populate_vector_store_from_parquet(
+        "/workspaces/codespaces-blank/data/generated_embeddings/combined_embeddings.parquet"
+    )
+    logger.info("Vector store populated with scraped data")
